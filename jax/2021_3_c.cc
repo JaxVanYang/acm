372c97555cd469f3f989437eb333dcf52819591e
Jax <jaxvanyang@gmail.com>
2021-03-03 21:35:30 +0800

    add comment

--- jax/2021_3_c.cc
+++ jax/2021_3_c.cc
@@ -11,20 +11,18 @@ int main() {
   cin >> s;
   vector<int> ps{0}, pos{0}, pois{0};
   for (auto &ch : s) {
+    // 记录前一个前缀和
     int pCnt = ps.back(), poCnt = pos.back(), poiCnt = pois.back();
-    ps.push_back(ps.back());
-    pos.push_back(pos.back());
-    pois.push_back(pois.back());
     if (ch == 'p')
-      ps.back() = (ps.back() + 1) % mod;
+      ps.push_back((ps.back() + 1) % mod);
     else if (ch == 'o')
-      pos.back() = (pos.back() + ps.back()) % mod;
+      pos.push_back((pos.back() + ps.back()) % mod);
     else if (ch == 'i')
-      pois.back() = (pos.back() + pois.back()) % mod;
+      pois.push_back((pos.back() + pois.back()) % mod);
     else if (ch == '*') {
-      ps.back() = (pCnt * 3 % mod + 1) % mod;
-      pos.back() = (poCnt * 3 % mod + pCnt) % mod;
-      pois.back() = (poiCnt * 3 % mod + poCnt) % mod;
+      ps.push_back((pCnt * 3 % mod + 1) % mod);
+      pos.push_back((poCnt * 3 % mod + pCnt) % mod);
+      pois.push_back((poiCnt * 3 % mod + poCnt) % mod);
     }
   }
   cout << pois.back() << endl;
