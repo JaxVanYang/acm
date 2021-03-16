67a3d753f336db39caa03885eab609c6cd07f49c
Jax Young <jaxvanyang@gmail.com>
2021-03-16 18:17:14 +0800

    ac code

--- 2021-trails/e.cc
+++ 2021-trails/e.cc
@@ -3,79 +3,73 @@ using namespace std;
 
 const int maxn = 5e4 + 5;
 
+int a[maxn];
 int n, m;
+
 struct Tree {
-  int l, r;
-  int cnt;
-  int mark = -1;
+  int l, r, cnt, mark = -1;
 } t[maxn * 4];
 
-int a[maxn];
-
-void pushup(int u) { t[u].cnt = t[u << 1].cnt + t[u << 1 | 1].cnt; }
-
-void pushdown(int u) {
-  if (t[u].mark == -1) return;
-  Tree &root = t[u], &left = t[u << 1], &right = t[u << 1 | 1];
-  left.mark = right.mark = root.mark;
-  left.cnt = (left.r - left.l + 1) * root.mark;
-  right.cnt = (right.r - right.l + 1) * root.mark;
-  root.mark = -1;
-}
-
-void build(int u, int l, int r) {
-  //   t[u].mark = -1;
-  t[u].l = l, t[u].r = r;
+void build(int p, int l, int r) {
+  t[p].l = l, t[p].r = r;
   if (l == r) {
-    t[u].cnt = a[l];
-  } else {
-    int mid = l + r >> 1;
-    build(u << 1, l, mid), build(u << 1 | 1, mid + 1, r);
-    pushup(u);
+    t[p].cnt = a[l];
+    return;
   }
+  int mid = l + r >> 1;
+  if (l <= mid) build(p << 1, l, mid);
+  if (mid < r) build(p << 1 | 1, mid + 1, r);
+  t[p].cnt = t[p << 1].cnt + t[p << 1 | 1].cnt;
 }
 
-void modify(int u, int l, int r, int v) {
-  if (l <= t[u].l && t[u].r <= r) {
-    t[u].cnt = (t[u].r - t[u].l + 1) * v;
-    t[u].mark = v;
-  } else {
-    pushdown(u);
-    int mid = t[u].l + t[u].r >> 1;
-    if (l <= mid) modify(u << 1, l, r, v);
-    if (r > mid) modify(u << 1 | 1, l, r, v);
-    pushup(u);
-  }
+void spread(int p) {
+  if (t[p].mark == -1) return;
+  Tree &l = t[p << 1], &r = t[p << 1 | 1];
+  l.mark = r.mark = t[p].mark;
+  l.cnt = (l.r - l.l + 1) * l.mark;
+  r.cnt = (r.r - r.l + 1) * r.mark;
+  t[p].mark = -1;
 }
 
-int query(int u, int l, int r) {
-  if (t[u].l >= l && t[u].r <= r) {
-    return t[u].cnt;
-  } else {
-    pushdown(u);
-    int res = 0;
-    int mid = t[u].l + t[u].r >> 1;
-    if (l <= mid) res += query(u << 1, l, r);
-    if (r > mid) res += query(u << 1 | 1, l, r);
-    return res;
+void change(int p, int l, int r, int v) {
+  if (l <= t[p].l && t[p].r <= r) {
+    t[p].cnt = (t[p].r - t[p].l + 1) * v;
+    t[p].mark = v;
+    return;
   }
+  spread(p);
+  int mid = t[p].l + t[p].r >> 1;
+  if (l <= mid) change(p << 1, l, r, v);
+  if (mid < r) change(p << 1 | 1, l, r, v);
+  t[p].cnt = t[p << 1].cnt + t[p << 1 | 1].cnt;
+}
+
+int ask(int p, int l, int r) {
+  if (l <= t[p].l && t[p].r <= r) return t[p].cnt;
+  spread(p);
+  int mid = t[p].l + t[p].r >> 1;
+  int ret = 0;
+  if (l <= mid) ret += ask(p << 1, l, r);
+  if (mid < r) ret += ask(p << 1 | 1, l, r);
+  return ret;
 }
 
 int main() {
-  scanf("%d%d", &n, &m);
-  for (int i = 1; i <= n; i++) scanf("%d", &a[i]);
+  cin >> n >> m;
+  for (int i = 1; i <= n; ++i) scanf("%d", a + i);
   build(1, 1, n);
-  for (int i = 0; i < m; i++) {
-    int a, b, c;
-    scanf("%d%d%d", &a, &b, &c);
-    int num = query(1, b, c);
-    if (a) {
-      modify(1, b, b + num - 1, 1), modify(1, b + num, c, 0);
+  // for (int i = 1; i <= n; ++i) printf("%d ", ask(1, i, i));
+  while (m--) {
+    int op, l, r;
+    scanf("%d%d%d", &op, &l, &r);
+    int cnt = ask(1, l, r);
+    if (op) {
+      change(1, l, l + cnt - 1, 1);
+      change(1, l + cnt, r, 0);
     } else {
-      modify(1, b, c - num, 0), modify(1, c - num + 1, c, 1);
+      change(1, l, r - cnt, 0);
+      change(1, r - cnt + 1, r, 1);
     }
   }
-  for (int i = 1; i <= n; i++) printf("%d ", query(1, i, i));
-
-  return 0;
+  for (int i = 1; i <= n; ++i) printf("%d ", ask(1, i, i));
 }
