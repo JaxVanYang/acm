962e593db854a2923ac3caa284d43c517854cfa0
Jax <jaxvanyang@gmail.com>
2021-03-26 14:13:58 +0800

    update ac code for acw101

--- share/algorithm-contest-advance-direction/0x00-basic/acw101.cc
+++ share/algorithm-contest-advance-direction/0x00-basic/acw101.cc
@@ -1,27 +1,32 @@
 #include <iostream>
-#include <unordered_map>
-#include <map>
+#include <set>
 using namespace std;
+
 const int maxn = 10005;
-// unordered_map 不能用 pair 作为 key，不知道为什么
-// unordered_map<pair<int, int>, bool> existed;
-map<pair<int, int>, bool> existed;
-int c[maxn], d[maxn];
+int a[maxn];    // 只使用一个数组节省存储空间
 
 int main() {
     int n, p, h, m;
-    cin >> n >> p >> h >> m;
-    for (int i = 0; i < m; ++i) {
-        int a, b;
-        scanf("%d%d", &a, &b);
-        if (a > b) swap(a, b);
-        if (existed[make_pair(a, b)]) continue;
-        // 将第 a + 1 到 b - 1 头牛降低高度
-        d[a + 1]--, d[b]++; // 差分变化
-        existed[make_pair(a, b)] = 1;
+    scanf("%d%d%d%d", &n, &p, &h, &m);
+    set<pair<int, int>> st;
+    while (m--) {
+        int x, y;
+        scanf("%d%d", &x, &y);
+        if (x > y) swap(x, y);  // 注意保持 x < y，如果颠倒会导致在差分序列上的操作与正常相反
+        auto xy = make_pair(x, y);
+        if (!st.count(xy)) {
+            st.insert(xy);
+            a[x + 1]--;
+            a[y]++;
+        }
     }
     for (int i = 1; i <= n; ++i) {
-        c[i] = c[i - 1] + d[i]; // 前一项加上这一项与前一项的差就是这一项的值
-        printf("%d\n", h + c[i]);
+        a[i] += a[i - 1]; // 前一项加上这一项与前一项的差就是这一项的值
+        printf("%d\n", h + a[i]);
     }
 }
+/*
+ * 书上的题解使用 map 来记录，这里使用了 set 来替代
+ * 另外值得注意的一点是：map 和 set 都能使用 pair 作为键，而 unordered_map 和 unordered_set 不行
+ * 具体原因待考
+ */
