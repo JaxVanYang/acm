518fc12078b5c0c0d46992507a25feac44e2d277
Jax <jaxvanyang@gmail.com>
2021-05-03 10:17:20 +0800

    add comment

--- jax/21-03-30-daily/e.cc
+++ jax/21-03-30-daily/e.cc
@@ -1,18 +1,19 @@
 #include <iostream>
-// #include <stack>
-#include <vector>
 using namespace std;
 using ll = long long;
 
 const int maxn = 3e5 + 5;
+// 记录每种可作为左边或右边的括号序列个数
 ll lft[maxn], rht[maxn];
 
+// 计算左括号与右括号个数的差值
 inline int count(const string &s) {
     int cnt0 = 0, cnt1 = 0;
     for (auto &ch : s) if (ch == '(') cnt0++; else cnt1++;
     return abs(cnt0 - cnt1);
 }
 
+// 判断是否可作为左边的括号序列
 inline bool isLeft(const string &s) {
     int cnt0 = 0, cnt1 = 0;
     for (int i = 0; i < s.length(); ++i) {
@@ -22,6 +23,7 @@ inline bool isLeft(const string &s) {
     return true;
 }
 
+// 判断是否可作为右边的括号序列
 inline bool isRight(const string &s) {
     int cnt0 = 0, cnt1 = 0;
     for (int i = s.length() - 1; i >= 0; --i) {
@@ -36,18 +38,16 @@ int main() {
     cin >> n;
     string s;
     for (int i = 0; i < n; ++i) {
-        // scanf("%s", s);
         cin >> s;
-        // cout << s << " : " << cnt << endl;
         if (isLeft(s)) {
             int cnt = count(s);
-            // cout << "left " << cnt << endl;
             lft[cnt]++;
+
+            // 如果差值为 0，说明即可作左边的序列也可以作为右边的序列
             if (cnt == 0) rht[0]++;
         } else if (isRight(s)) {
             int cnt = count(s);
             rht[cnt]++;
-            // cout << "right " << cnt << endl;
         }
     }
 
