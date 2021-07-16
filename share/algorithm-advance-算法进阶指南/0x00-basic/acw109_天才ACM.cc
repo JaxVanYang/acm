fc40ccc9b9a35a0c741911c256b94b3838ebaa9b
Jax <jaxvanyang@gmail.com>
2021-07-16 17:25:21 +0800

    update solution: acw109_天才ACM.cc

--- share/algorithm-advance-算法进阶指南/0x00-basic/acw109_天才ACM.cc
+++ share/algorithm-advance-算法进阶指南/0x00-basic/acw109_天才ACM.cc
@@ -1,82 +1,74 @@
 #include <iostream>
 #include <cstring>
 #include <algorithm>
-#include <queue>
-#include <cmath>
-
 using namespace std;
 using ll = long long;
 
-const int maxn = 5e5 + 5;
-// a: original array; b: array used for calculation; c: temporary array
-int a[maxn], b[maxn], c[maxn];
+const int N = 5e5;
 
-ll calc(int l, int r, int m) {
+// a: original; b: sorted; c: temporary
+int a[N], b[N], c[N];
+
+// lmt: the right bound of b
+int n, m, lmt;
+ll t;
+
+inline ll f(int x, int y) {
+    ll diff = y - x;
+    return diff * diff;
+}
+
+// merge the old section b[l : lmt] and new new section b[lmt : r] to c[l : r]
+void merge(int l, int r) {
+    int i = l, j = lmt;
+    for (int k = l; k < r; ++k) {
+        if (j >= r || (i < lmt && b[i] <= b[j])) {
+            c[k] = b[i++];
+        } else {
+            c[k] = b[j++];
+        }
+    }
+}
+
+ll calc(int l, int r) {
+    if (r > n) r = n;
+    // cout << "l = " << l << " r = " << r << endl;
+    // cout << "lmt = " << lmt << endl;
     ll ret = 0;
-    while (m-- && l < r) {
-        ret += pow(b[l++] - b[r--], 2);
+    int len = r - l;
+    // copy the new section a[lmt : r] to b[lmt : r]
+    memcpy(b + lmt, a + lmt, sizeof(int) * (r - lmt));
+    sort(b + lmt, b + r);
+    merge(l, r);
+    for (int i = 0; i < min(m, len / 2); ++i) {
+        ret += f(c[l + i], c[r - 1 - i]);
     }
     return ret;
 }
 
-void merge(int l, int r, int p) {
-    // (r, r + p] is on the right of the section, so no need to worry about that merging would cover
-    // (r, r + p] is the new section, so it's from original array a
-    memcpy(b + r + 1, a + r + 1, sizeof(int) * p);
-    // [l, r] is on the left, so it must be copied to temporary array c
-    // b[l, r] is maintained to be always sorted.
-    memcpy(c + l, b + l, sizeof(int) * (r - l + 1));
-    
-    sort(b + r + 1, b + r + p + 1);
-    int i = l, j = r + 1, k = l;
-    for (; i <= r && j <= r + p; ++k) {
-        if (c[i] < b[j]) {
-            b[k] = c[i++];
-        } else {
-            b[k] = b[j++];
-        }
-    }
-    while (i <= r) {
-        b[k++] = c[i++];
-    }
-}
-
 void solve() {
-    int n, m;
-    ll t;
     scanf("%d%d%lld", &n, &m, &t);
-    for (int i = 0; i < n; ++i) {
-        scanf("%d", a + i);
-    }
+    lmt = 0;    // b 中已排序的边界
+    for (int i = 0; i < n; ++i) scanf("%d", a + i);
     int cnt = 0;
-    int l = 0;
+    int l = 0, r = 0;
     while (l < n) {
-        int p = 1, r = l;
-        // Note: Don't forget this!
-        b[l] = a[l];
         ++cnt;
-        // cout << "l = " << l << endl;
+        int p = 1;
         while (p != 0) {
-            if (r + p >= n) {
-                merge(l, r, n - r - 1);
-                if (calc(l, n - 1, m) <= t) {
-                    r = n - 1;
-                    break;
-                }
-                memcpy(b + l, c + l, sizeof(int) * (r - l + 1));
-                p /= 2;
-            }
-            merge(l, r, p);
-            if (calc(l, r + p, m) <= t) {
-                r += p;
+            // cout << "l = " << l << " r = " << r << " p = " << p << endl;
+            ll val = calc(l, r + p);
+            // cout << "num = " << val << endl;
+            if (val <= t) {
+                lmt = r = min(r + p, n);
+                if (r == n) break;
                 p *= 2;
+                memcpy(b + l, c + l, sizeof(int) * (lmt - l));
             } else {
-                memcpy(b + l, c + l, sizeof(int) * (r - l + 1));
                 p /= 2;
             }
         }
-        // cout << "r = " << r << endl;
-        l = r + 1;
+        l = r;
     }
     printf("%d\n", cnt);
 }
