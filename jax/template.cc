fc53705232b439a66977a62fe1e8b5cfcdd2ab2e
Jax <jaxvanyang@gmail.com>
2021-07-23 15:01:57 +0800

    add template.cc for CP & solve problem

---
+++ jax/template.cc
@@ -0,0 +1,24 @@
+// You can create a snippet for this in VS Code
+
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using vi = vector<int>;
+using pi = pair<int, int>;
+
+#define REP(i, l, r) for (int i = l; i < r; ++i)
+#define F first
+#define S second
+#define PF push_fornt
+#define PB push_back
+#define MP make_pair
+
+const char el = '\n';
+const int N = 1e6 + 10;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    
+}
