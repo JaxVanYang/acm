62182a1628639e295e463b7a8ac9bfa382898182
Jax <jaxvanyang@gmail.com>
2021-07-23 19:49:16 +0800

    solve problem & add verbose template for CP

---
+++ jax/templates/verbose_template.cc
@@ -0,0 +1,39 @@
+// Verbose template for CP
+// You can create a snippet for this in VS Code
+
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+using vi = vector<int>;
+using pi = pair<int, int>;
+
+#define REP(i, l, r) for (int i = l; i < r; ++i)
+#define F first
+#define S second
+#define PF push_fornt
+#define PB push_back
+#define MP make_pair
+#define Sort(v) sort(v.begin(), v.end())
+#define Unique(v) unique(v.begin(), v.end())
+#define Lower(v, val) lower_bound(v.begin(), v.end(), val)
+#define Upper(v, val) upper_bound(v.begin(), v.end(), val)
+#define Reverse(v) reverse(v.begin(), v.end())
+
+const char el = '\n';
+const int N = 1e6 + 10;
+
+template<typename T>
+void print(T &v) {
+    for (auto val : v) {
+        cout << val << ' ';
+    }
+    cout << el;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    
+}
