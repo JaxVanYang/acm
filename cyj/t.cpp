1cdc881ba06e4290d5387c81e57441cb964cc87d
cyj <2650426179@qq.com>
2021-11-01 22:41:50 +0800

    test

--- cyj/t.cpp
+++ cyj/t.cpp
@@ -5,7 +5,7 @@
  * @Status: 
  * @Author: cyj
  * @Date: 2021-11-01 09:18:01
- * @LastEditTime: 2021-11-01 09:21:16
+ * @LastEditTime: 2021-11-01 22:24:13
  */
 
 #include <iostream>
@@ -13,14 +13,7 @@
 using namespace std;
 
 int main(){
-    string a, b;
-    char c[100];
-    cin >> a >> b;
-    int cnt = 0;
-    for (int i = 0, j = 0; i < a.size(); i ++, j = (j+1) % b.size()){
-        c[cnt ++] = ((a[i] - 'a') + (b[j] - 'a') ) % 26 + 'a';
-    }
-    for (int i = 0; i < cnt; i ++) cout << c[i];
+    cout << "hello world\n";
 
     return 0;
 }
